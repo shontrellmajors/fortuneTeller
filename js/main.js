28c99ded9c9bcf1e6bd3ff7ae7f5b9c7c55fa463
Shontrell Majors <smajors@pivottechschool.com>
2020-10-07 17:21:49 -0500

    added js, css, html

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*Why pay a fortune teller when you can just program your fortune yourself?
+
+Store the following into variables: number of children, partner's name, geographic location, job title.
+Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids"*/
+
+var numOfChild = 3;
+let partnerName = "Trevor";
+let geolocation = "Nashville";
+let jobTitle = "Software Engineer";
+
+console.log("You will be a " + jobTitle + " in " + geolocation + ", and married to " + partnerName + " with " + numOfChild + " kids.");
